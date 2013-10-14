784806f291db53bae0b8ca9b05b638f181fb47db
Emil Aura <aura.emil@gmail.com>
2013-10-14 14:46:20 +0200

    2.G29_0001: Added an individual salt to every password, the old "general salt" is used as a pepper

--- src/amu/action/ChangePasswordAction.java
+++ src/amu/action/ChangePasswordAction.java
@@ -4,13 +4,18 @@ import amu.Authentication;
 import amu.Utils;
 import amu.database.CustomerDAO;
 import amu.model.Customer;
+import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 class ChangePasswordAction implements Action {
+
+    private CustomerDAO customerDAO;
 
     @Override
     public ActionResponse execute(HttpServletRequest request, HttpServletResponse response) {
@@ -27,7 +32,8 @@ class ChangePasswordAction implements Action {
             List<String> messages = new ArrayList<String>();
             request.setAttribute("messages", messages);
             String oldPassword = request.getParameter("oldPassword");
-            if (!Authentication.hashPassword(oldPassword).equals(customer.getPassword())) {
+            String salt = customerDAO.getSalt(customer.getId());
+            if (!Authentication.hashPassword(oldPassword, salt).equals(customer.getPassword())) {
                 messages.add("Wrong old password");
                 return new ActionResponse(ActionResponseType.FORWARD, "changePassword");
             }
@@ -38,16 +44,23 @@ class ChangePasswordAction implements Action {
                 messages.add("Password and repeated password did not match. Please try again.");
                 return new ActionResponse(ActionResponseType.FORWARD, "changePassword");
             }
-            
+
             // Validate that new email complexity
             if (!Utils.validatePassword(password[0])) {
-                           messages.add("Password must be between 8 and 40 charactes long and contain: a special character(@#$%^&+=), an upper case letter, a lower case letter and a digit ");
+                messages.add("Password must be between 8 and 40 charactes long and contain: a special character(@#$%^&+=), an upper case letter, a lower case letter and a digit ");
                 return new ActionResponse(ActionResponseType.FORWARD, "changePassword");
             }
 
             // Validation OK, do business logic
-            CustomerDAO customerDAO = new CustomerDAO();
-            customer.setPassword(Authentication.hashPassword(password[0]));
+            try {
+                salt = Authentication.generateSalt();
+                customer.setSalt(salt);
+                customer.setPassword(Authentication.hashPassword(password[0], salt));
+            } catch (NoSuchAlgorithmException ex) {
+                Logger.getLogger(ChangePasswordAction.class.getName()).log(Level.SEVERE, null, ex);
+                messages.add("An error occured.");
+                return new ActionResponse(ActionResponseType.FORWARD, "changePassword");
+            }
             if (customerDAO.edit(customer) == false) {
                 messages.add("An error occured.");
                 return new ActionResponse(ActionResponseType.FORWARD, "changePassword");
--- src/amu/action/LoginCustomerAction.java
+++ src/amu/action/LoginCustomerAction.java
@@ -16,86 +16,89 @@ import amu.model.Customer;
 
 class LoginCustomerAction implements Action {
 
-	@Override
-	public ActionResponse execute(HttpServletRequest request,
-			HttpServletResponse response) throws Exception {
-
-		Map<String, String> values = new HashMap<String, String>();
-
-		request.setAttribute("values", values);
-		if (ActionFactory.hasKey(request.getParameter("from"))) {
-			values.put("from", request.getParameter("from"));
-		}
-
-		if (request.getMethod().equals("POST")) {
-			return authenticateUser(request, values);
-		}
-
-		return new ActionResponse(ActionResponseType.FORWARD, "loginCustomer");
-	}
-
-	private ActionResponse authenticateUser(HttpServletRequest request,
-			Map<String, String> values) {
-                request.getSession().invalidate();
-		HttpSession session = request.getSession(true);
-		String email = request.getParameter("email");
-		String passwd = request.getParameter("password");
-
-		Map<String, String> messages = new HashMap<String, String>();
-		request.setAttribute("messages", messages);
-		Integer count = (Integer) session.getAttribute("loginCount");
-		count=count== null ? 1 : count;
-		session.setAttribute("loginCount", count+1);
-		
-		if (count>=3 && !validateCaptcha(request)) {
-			messages.put("email", "Wrong captcha.");
-			return new ActionResponse(ActionResponseType.FORWARD,
-					"loginCustomer");
-		}
-
-		CustomerDAO customerDAO = new CustomerDAO();
-		Customer customer = customerDAO.findByEmail(request
-				.getParameter("email"));
-
-		if (customer != null) {
-			if (customer.getActivationToken() == null) {
-				if (checkPasswd(passwd, customer)) {
-					session.setAttribute("loginCount",0);
-					session.setAttribute("customer", customer);
-					if (ActionFactory.hasKey(request.getParameter("from"))) {
-						return new ActionResponse(ActionResponseType.REDIRECT,
-								request.getParameter("from"));
-					}
-				}
-			} else { // customer.getActivationToken() != null
-				return new ActionResponse(ActionResponseType.REDIRECT,
-						"activateCustomer");
-			}
-		}
-		messages.put("email", "Incorrect login details.");
-
-		// Forward to login form with error messages
-		return new ActionResponse(ActionResponseType.FORWARD, "loginCustomer");
-	}
-
-	private boolean validateCaptcha(HttpServletRequest request) {
-
-		String remoteAddr = request.getRemoteAddr();
-		ReCaptchaImpl reCaptcha = new ReCaptchaImpl();
-		reCaptcha.setPrivateKey(Config.RECAPTCHA_PRIVATE_KEY);
-
-		String challenge = request.getParameter("recaptcha_challenge_field");
-		String uresponse = request.getParameter("recaptcha_response_field");
-                if(challenge!=null && uresponse!=null){
-                    ReCaptchaResponse reCaptchaResponse = reCaptcha.checkAnswer(remoteAddr,
-                                    challenge, uresponse);
-                    return reCaptchaResponse.isValid();
+    private CustomerDAO customerDAO;
+    
+    @Override
+    public ActionResponse execute(HttpServletRequest request,
+            HttpServletResponse response) throws Exception {
+        
+        Map<String, String> values = new HashMap<String, String>();
+        
+        request.setAttribute("values", values);
+        if (ActionFactory.hasKey(request.getParameter("from"))) {
+            values.put("from", request.getParameter("from"));
+        }
+        
+        if (request.getMethod().equals("POST")) {
+            return authenticateUser(request, values);
+        }
+        
+        return new ActionResponse(ActionResponseType.FORWARD, "loginCustomer");
+    }
+    
+    private ActionResponse authenticateUser(HttpServletRequest request,
+            Map<String, String> values) {
+        request.getSession().invalidate();
+        HttpSession session = request.getSession(true);
+        String email = request.getParameter("email");
+        String passwd = request.getParameter("password");
+        
+        Map<String, String> messages = new HashMap<String, String>();
+        request.setAttribute("messages", messages);
+        Integer count = (Integer) session.getAttribute("loginCount");
+        count = count == null ? 1 : count;
+        session.setAttribute("loginCount", count + 1);
+        
+        if (count >= 3 && !validateCaptcha(request)) {
+            messages.put("email", "Wrong captcha.");
+            return new ActionResponse(ActionResponseType.FORWARD,
+                    "loginCustomer");
+        }
+        
+        CustomerDAO customerDAO = new CustomerDAO();
+        Customer customer = customerDAO.findByEmail(request
+                .getParameter("email"));
+        
+        if (customer != null) {
+            if (customer.getActivationToken() == null) {
+                if (checkPasswd(passwd, customer)) {
+                    session.setAttribute("loginCount", 0);
+                    session.setAttribute("customer", customer);
+                    if (ActionFactory.hasKey(request.getParameter("from"))) {
+                        return new ActionResponse(ActionResponseType.REDIRECT,
+                                request.getParameter("from"));
+                    }
                 }
-                
-            return false;
-	}
+            } else { // customer.getActivationToken() != null
+                return new ActionResponse(ActionResponseType.REDIRECT,
+                        "activateCustomer");
+            }
+        }
+        messages.put("email", "Incorrect login details.");
 
-	private boolean checkPasswd(String passwd, Customer customer) {
-		return customer.getPassword().equals(Authentication.hashPassword(passwd));
-	}
+        // Forward to login form with error messages
+        return new ActionResponse(ActionResponseType.FORWARD, "loginCustomer");
+    }
+    
+    private boolean validateCaptcha(HttpServletRequest request) {
+        
+        String remoteAddr = request.getRemoteAddr();
+        ReCaptchaImpl reCaptcha = new ReCaptchaImpl();
+        reCaptcha.setPrivateKey(Config.RECAPTCHA_PRIVATE_KEY);
+        
+        String challenge = request.getParameter("recaptcha_challenge_field");
+        String uresponse = request.getParameter("recaptcha_response_field");
+        if (challenge != null && uresponse != null) {
+            ReCaptchaResponse reCaptchaResponse = reCaptcha.checkAnswer(remoteAddr,
+                    challenge, uresponse);
+            return reCaptchaResponse.isValid();
+        }
+        
+        return false;
+    }
+    
+    private boolean checkPasswd(String passwd, Customer customer) {
+        customerDAO = new CustomerDAO();
+        return customer.getPassword().equals(Authentication.hashPassword(passwd, customerDAO.getSalt(customer.getId())));
+    }
 }
