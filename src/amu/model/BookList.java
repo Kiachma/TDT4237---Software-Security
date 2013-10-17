01393a5594eaf509e06b45fc3951b236c50c90b2
Emil Aura <aura.emil@gmail.com>
2013-10-17 14:35:19 +0200

    Added the models neede to extend the application

---
+++ src/amu/model/BookList.java
@@ -0,0 +1,45 @@
+package amu.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BookList {
+
+    public String title;
+    public String description;
+    public List<BookListItem> books;
+
+    public List getBooks() {
+        return books;
+    }
+
+    public void addBook(Book book) {
+        if (books == null) {
+            books = new ArrayList<>();
+        }
+        BookListItem bookListItem = new BookListItem();
+        bookListItem.setBook(book);
+        bookListItem.setList(this);
+        books.add(bookListItem);
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setBooks(List<BookListItem> books) {
+        this.books = books;
+    }
+}
