c099c0d614eb73e5378c759c23d5f3ea7a119f86
bjorfoss <bjofoss2@hotmail.com>
2013-10-17 17:04:59 +0200

    Add password check in delete address page, avoid parsing error

--- src/amu/action/DeleteAddressAction.java
+++ src/amu/action/DeleteAddressAction.java
@@ -1,5 +1,6 @@
 package amu.action;
 
+import amu.Authentication;
 import amu.database.AddressDAO;
 import amu.model.Address;
 import amu.model.Customer;
@@ -14,7 +15,23 @@ class DeleteAddressAction implements Action {
     @Override
     public ActionResponse execute(HttpServletRequest request, HttpServletResponse response) {
         HttpSession session = request.getSession(true);
+        List<String> messages = new ArrayList<String>();
+        request.setAttribute("messages", messages);
+        
         Customer customer = (Customer) session.getAttribute("customer");
+        
+        String errors = request.getParameter("e");
+        if (errors != null) {
+        	switch(errors) {
+        	case "auth" : {
+        		messages.add("Authentication error. Please try again.");
+        		break;
+        	}
+        	default : {
+        		messages.add("An error occured");
+        	}
+        	}
+        }
 
         if (customer == null) {
             ActionResponse actionResponse = new ActionResponse(ActionResponseType.REDIRECT, "loginCustomer");
@@ -25,19 +42,37 @@ class DeleteAddressAction implements Action {
         AddressDAO addressDAO = new AddressDAO();
         Address address;
 
-        if (request.getMethod().equals("POST")) {
-            List<String> messages = new ArrayList<String>();
-            request.setAttribute("messages", messages);
-
-            if (addressDAO.delete(Integer.parseInt(request.getParameter("id")))) {
-                return new ActionResponse(ActionResponseType.REDIRECT, "viewCustomer");
+        int addrID = -1;
+        try {
+        	addrID = Integer.parseInt(request.getParameter("id"));
+        } catch (Exception e) {}//ignore
+        
+		if (request.getMethod().equals("POST")) {
+            
+            
+         // Validate password
+            String password = request.getParameter("password");
+            boolean matches = Authentication.verifyPassword(customer, password);
+            if (!matches) {
+            	ActionResponse r = new ActionResponse(ActionResponseType.REDIRECT, "deleteAddress");
+            	r.addParameter("id", "" + addrID);
+            	r.addParameter("e", "auth");
+            	return r;
             }
 
-            messages.add("An error occured."); 
+            if (addrID >= 0 && addressDAO.delete(addrID)) {
+                return new ActionResponse(ActionResponseType.REDIRECT, "viewCustomer");
+            } else {
+            	messages.add("An error occured.");
+            	return new ActionResponse(ActionResponseType.FORWARD, "deleteAddress");
+            }
         }
 
         // (request.getMethod().equals("GET")) 
-        address = addressDAO.read(Integer.parseInt(request.getParameter("id"))); 
+		if (addrID < 0) {
+			return new ActionResponse(ActionResponseType.REDIRECT, "viewCustomer");
+		}
+        address = addressDAO.read(addrID); 
         request.setAttribute("address", address);
         return new ActionResponse(ActionResponseType.FORWARD, "deleteAddress");
     }
